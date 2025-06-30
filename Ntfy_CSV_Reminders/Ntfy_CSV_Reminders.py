e79f9516f7a4ff2c11fe94349c8254f8ee37f134
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-06-30 23:45:52 +0200

    bump version 0.1.2 -> 0.1.3

--- Ntfy_CSV_Reminders/Ntfy_CSV_Reminders.py
+++ Ntfy_CSV_Reminders/Ntfy_CSV_Reminders.py
@@ -7,7 +7,7 @@ import time
 from typing import Union, List, Tuple, Dict
 
 class NtfyCSVReminders:
-    __VERSION__: str = "0.1.2"
+    __VERSION__: str = "0.1.3"
 
     def __init__(
         self,
